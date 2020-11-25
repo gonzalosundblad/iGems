e9edf7284edd3500e9fc0d6ca6fff451bff1e6a7
gonzalo <sundbladgonzalo@gmail.com>
2020-11-25 09:46:22 -0300

    gitignores

--- api/src/app.js
+++ api/src/app.js
@@ -7,9 +7,6 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 
 
-
-
-
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 // server.use(cookieParser('secret'));
